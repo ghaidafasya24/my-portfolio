c46253c7dffe104771d2463d1a1c40d9ee352496
ghaidafasya24 <ghaidafasya5@gmail.com>
2024-12-24 20:54:06 +0700

    refactor contact form to use EmailJS for email sending and streamline form submission handling

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+const btn = document.getElementById('button');
+
+document.getElementById('contact-form').addEventListener
+('submit', function(event) {
+    event.preventDefault();
+
+    btn.value = 'Sending...';
+
+    const serviceID =  'service_2dlij3r';
+    const templateID = 'template_cw7cj7r';
+
+
+    emailjs.sendForm(serviceID, templateID, this)
+        .then(() => {
+            btn.value = 'Send Email';
+            alert('Email berhasil terkirim!');
+        }, (err) => {
+            btn.value = 'Send Email';
+            alert(JSON.stringify(err));
+        });
+});
