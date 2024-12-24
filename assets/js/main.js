b1ef674e77e03bf73b7e57cd0bee7b3c81f4819e
ghaidafasya24 <ghaidafasya5@gmail.com>
2024-12-24 21:29:22 +0700

    refactor contact form submission to reset inputs after successful email send

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,20 @@
 const btn = document.getElementById('button');
 
-document.getElementById('contact-form').addEventListener
-('submit', function(event) {
+document.getElementById('contact-form').addEventListener('submit', function(event) {
     event.preventDefault();
 
     btn.value = 'Sending...';
 
-    const serviceID =  'service_2dlij3r';
+    const serviceID = 'service_2dlij3r';
     const templateID = 'template_cw7cj7r';
-
 
     emailjs.sendForm(serviceID, templateID, this)
         .then(() => {
             btn.value = 'Send Email';
             alert('Email berhasil terkirim!');
+
+            // Clear the form inputs after successful send
+            document.getElementById('contact-form').reset();
         }, (err) => {
             btn.value = 'Send Email';
             alert(JSON.stringify(err));
